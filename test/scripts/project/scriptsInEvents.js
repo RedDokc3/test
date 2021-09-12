69c68d93115a880f71fdb6e41ce50d62ae67420a
RedDokc3 <90576942+RedDokc3@users.noreply.github.com>
2021-09-12 23:42:53 +0300

    Add files via upload

---
+++ test/scripts/project/scriptsInEvents.js
@@ -0,0 +1,20 @@
+
+
+
+const scriptsInEvents = {
+
+		async EventSheet1_Event1(runtime, localVars)
+		{
+			src="/vkbridge.js"
+			bridge.send("VKWebAppInit");
+			bridge.send("VKWebAppGetUserInfo");
+			bridge.subscribe((e) => {
+			if(e.type == "VKWebAppGetUserInfoResult") {
+			   console.log(e.data.status);
+			}});
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
